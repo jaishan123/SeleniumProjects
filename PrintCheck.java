32f688de09ac51b0c87e47340e28b9781ab5faf9
jaishan123 <86280754+jaishan123@users.noreply.github.com>
2022-04-28 12:10:52 +0530

    Add files via upload
    3 files

---
+++ PrintCheck.java
@@ -0,0 +1,12 @@
+
+public class PrintCheck extends HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+PrintCheck PC=new PrintCheck();
+System.out.println("Jaishankar");
+PC.check(args);
+
+}
+}
---
+++ helloworld_child.java
@@ -0,0 +1,9 @@
+
+public class helloworld_child extends HelloWorld {
+	public static void main(String[] args) {
+		
+		helloworld_child hwchild=new helloworld_child();
+		hwchild.check(args);
+		System.out.println("Hello Child");
+		
+}}
